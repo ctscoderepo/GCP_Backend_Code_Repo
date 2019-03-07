e4c0dc2475f7e4de7885e855dbe13edbbc47fe12
ctscoderepo <anujgupta1403@gmail.com>
2019-03-07 14:39:55 +0530

    Remove CORS headers from response.

--- registration-service/src/main/java/com/gcp/registration/controller/UserloginController.java
+++ registration-service/src/main/java/com/gcp/registration/controller/UserloginController.java
@@ -4,11 +4,9 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +21,6 @@ import com.gcp.registration.service.UserService;
  * 
  * This class is rest controller which publish end point.
  */  
-
-@CrossOrigin(origins = "http://104.154.86.111") 
 @RestController
 @RequestMapping("/api")
 public class UserloginController {
