88afbf2479e097c0d3dbde3eaec63d74399198a5
ctscoderepo <anujgupta1403@gmail.com>
2019-03-29 16:31:33 +0530

    Added webroot name in all api end points

--- search-service/src/main/java/com/gcp/demo/api/CategoryController.java
+++ search-service/src/main/java/com/gcp/demo/api/CategoryController.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-@RestController("/api")
+@RestController("/category")
 public class CategoryController {
 
     private ObjectMapper mapper = new ObjectMapper();
